9d9403a92fb5cccce16a353355817a8a6bda220d
Helenapati <88396481+Helenapati@users.noreply.github.com>
2021-12-09 10:07:50 -0300

    Aluna: Helena Patrícia de Souza Silva
    Turma: 2A

---
+++ Main (9).java
@@ -0,0 +1,79 @@
+ public class {
+     
+     public static void main (String args []){
+         
+         Map<String, String> professor = new.HashMap<String, String>();
+         
+         professor.put ("João", new String ("Professor")); 
+         professor.put ("Maria", new String ("Professor")); 
+         
+         String keyToSearch = professor; 
+         
+         if ( professor.containsKey (keyToSearch)) {
+             
+            System.out.println(keyToSearch + professor.get(keyToSearch);
+         }
+            
+            HashSet<Salario> dset = new HashSet<Salario> ();
+            dset.add(new Salario(1,200); 
+            dset.add(new Salario(1,400);
+            Iterator<Salario> iterator = dset.iterator();
+             while (iterator.hashNext()) {
+                 System.out.println(iterstor.next());
+     
+             }
+        HashSet<CPF> dset = new HashSet<CPF> ();
+            dset.add(new CPF(123.654.987,52); 
+            dset.add(new CPF(789.456.321,65);
+            Iterator<Salario> iterator = dset.iterator();
+             while (iterator.hashNext()) {
+                 System.out.println(iterstor.next());
+             }
+             
+          Map<String, String> professor = new.HashMap<String, String>();
+         
+         professor.put ("João", new String ("Carlos")); 
+         professor.put ("Maria", new String ("Julia")); 
+         
+         String keyToSearch = professor; 
+         
+         if ( professor.containsKey (keyToSearch)) {
+             
+            System.out.println(keyToSearch + professor.get(keyToSearch);
+         }
+            
+            HashSet<Salario> dset = new HashSet<Salario> ();
+            dset.add(new Salario(2,500); 
+            dset.add(new Salario(2,700);
+            Iterator<Salario> iterator = dset.iterator();
+             while (iterator.hashNext()) {
+                 System.out.println(iterstor.next());
+     
+             }
+        HashSet<CPF> dset = new HashSet<CPF> ();
+            dset.add(new CPF(123.654.987,63); 
+            dset.add(new CPF(789.456.321,01);
+            Iterator<Salario> iterator = dset.iterator();
+             while (iterator.hashNext()) {
+                 System.out.println(iterstor.next());
+    
+            }
+        ArrayList<ArrayTest> test = new   ArrayList<ArrayTest>(){
+        ArrayTest = professor; 
+        professor = ArrayTest(1);
+        test.add(professor);
+        professor = ArrayTest(2);
+        test.add(professor);
+     }
+      getProductBycode (Interagir ProductCode(professor));   
+     }
+       getProductByCode (Double CPF){
+           System.out.println(CPF);
+       }
+       List<Interagir> list = new ArrayList<Interagir> (){
+           list.add (new "Carlos", new double 123,654,987,63);
+           list.add (new "Carlos", new double 789,456,321,01);
+           System.out.println(list);
+       }
+       
+ }
